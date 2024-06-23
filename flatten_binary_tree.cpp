be270276cb4d1fc1fd753bc664ec8a6d94b94a69
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-23 16:48:09 +0530

    Create flatten_binary_tree.cpp

---
+++ flatten_binary_tree.cpp
@@ -0,0 +1,33 @@
+// 1. Using stack push right first and left second so left will process first and right after that
+// time: O(N)
+// space: O(N)
+void flatten(TreeNode* root){
+  stack<TreeNode*> st;
+  st.push(root);
+  while(!st.empty()){
+    TreeNode *temp = st.top();
+    st.pop();
+    if(temp->right) st.push(temp->right);
+    if(temp->left) st.push(temp->left);
+    root->right = temp;
+    root->left = NULL;
+  }
+}
+
+// 2. (Best) use moris traversal: and attech the left's rightmost to the current's right node and fix the cur to the cur->right;
+// space: O(1)
+// time: O(N)
+void flatten(TreeNode* root) {
+  if(root==NULL) return;
+  TreeNode *cur = root;
+  while(cur){
+      if(cur->left){
+          TreeNode *temp = cur->left;
+          while(temp->right) temp = temp->right;
+          temp->right = cur->right;
+          cur->right = cur->left;
+          cur->left = NULL;
+      }
+      cur = cur->right;
+  }
+}
