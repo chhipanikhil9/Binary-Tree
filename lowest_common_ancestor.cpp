b10a4f2765db39e3025bd41f8d480328eb7cd032
Nikhil Chhipa <chhipanikhil9@gmail.com>
2024-06-24 13:15:21 +0530

    Update lowest_common_ancestor.cpp

--- lowest_common_ancestor.cpp
+++ lowest_common_ancestor.cpp
@@ -5,7 +5,8 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
     if(root==NULL || root==p || root== q) return root;
     TreeNode *l = lowestCommonAncestor(root->left,p,q);
     TreeNode *r = lowestCommonAncestor(root->right,p,q);
-    if(l==NULL) return r;
+    if(l==NULL and r==NULL) return NULL;
+    else if(l==NULL) return r;
     else if(r==NULL) return l;
     else return root;
 }
